18d8a81faac37a5f20edb40f7bf8736724fad7ff
Diana Thayer <me@dianathayer.org>
2016-12-31 18:32:51 -0800

    update -j, -v options and readme

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,8 @@ var pkg = require('../package.json')
 
 function eventToShortCode (codes, event) {
   var code = codes[event[0]]
-  var daysTil = (event[1].getTime() - Date.now()) / 1000 / 60 / 60 / 24
+  var date = new Date(event[1])
+  var daysTil = (date.getTime() - Date.now()) / 1000 / 60 / 60 / 24
   var sep = (daysTil < 0) ? '+' : ''
   return [code, Math.floor(-daysTil)].join(sep)
 }
@@ -22,45 +23,44 @@ program
 .parse(process.argv)
 
 var date = program.args[0] ? new Date(program.args[0]) : new Date() // TODO accept argv[1] as date
-var solarFuncs = [witch.solar.soonest, witch.solar.recent]
-var lunarFuncs = [witch.lunar.soonest, witch.lunar.recent]
-if (program.json) {
-  var events = {
-    solar: {
-      soonest: toSolar(witch.solar.soonest(date)),
-      recent: toSolar(witch.solar.recent(date))
-    },
-    lunar: {
-      soonest: toLunar(witch.lunar.soonest(date)),
-      recent: toLunar(witch.lunar.recent(date))
-    }
+var events = {
+  solar: {
+    soonest: witch.solar.soonest(date),
+    recent: witch.solar.recent(date)
+  },
+  lunar: {
+    soonest: witch.lunar.soonest(date),
+    recent: witch.lunar.recent(date)
   }
-  console.log(JSON.stringify(events))
-} else if (program.verbose) {
-  solarFuncs.forEach(function (solarFunc) {
-    var event = solarFunc(date)
-    var code = toSolar(event)
-    console.log(code, '@', event[1])
-  })
+}
 
-  lunarFuncs.forEach(function (lunarFunc) {
-    var event = lunarFunc(date)
-    var code = toLunar(event)
-    console.log(code, '@', event[1])
+var eventDetails = {}
+Object.keys(events).forEach(function (eventType) {
+  eventDetails[eventType] = {}
+  Object.keys(events[eventType]).forEach(function (funcType) {
+    var event = events[eventType][funcType]
+    var details = {
+      code: eventToShortCode(witch[eventType].CODES, event),
+      name: event[0],
+      date: event[1]
+    }
+    eventDetails[eventType][funcType] = details
   })
+})
+
+if (program.json) {
+  console.log(JSON.stringify(eventDetails))
 } else {
-  var codes = []
-  solarFuncs.forEach(function (solarFunc) {
-    var event = solarFunc(date)
-    var code = toSolar(event)
-    codes.push(code)
+  var toPrint = []
+  Object.keys(events).forEach(function (eventType) {
+    Object.keys(events[eventType]).forEach(function (funcType) {
+      var detail = eventDetails[eventType][funcType]
+      if (program.verbose) {
+        console.log(detail.code, '|', detail.name, '@', detail.date)
+      } else {
+        toPrint.push(detail.code)
+      }
+    })
   })
-
-  lunarFuncs.forEach(function (lunarFunc) {
-    var event = lunarFunc(date)
-    var code = toLunar(event)
-    codes.push(code)
-  })
-
-  console.log(codes.join(', '))
+  if (toPrint.length) console.log(toPrint.join(', '))
 }
--- readme.md
+++ readme.md
@@ -29,20 +29,55 @@ console.log(event)
 ```
 $ witch-clock
 VE-80, WS+9, FIRST-6, NEW+1
+
 $ witch-clock -v
-VE-80 @ 2017-03-20T10:28:52.704Z
-WS+9 @ 2016-12-21T10:44:19.494Z
-FIRST-6 @ 2017-01-05T19:46:59.933Z
-NEW+1 @ 2016-12-29T06:53:18.964Z
+VE-79 | vernal equinox @ 2017-03-20T10:28:52.704Z
+WS+10 | winter solstice @ 2016-12-21T10:44:19.494Z
+FIRST-5 | first @ 2017-01-05T19:46:59.933Z
+NEW+2 | new @ 2016-12-29T06:53:18.964Z
+
+$ witch-clock -j
+{"solar":{"soonest":{"code":"VE-79","name":"vernal equinox","date":"2017-03-20T10:28:52.704Z"},"recent":{"code":"WS+10","name":"winter solstice","date":"2016-12-21T10:44:19.494Z"}},"lunar":{"soonest":{"code":"FIRST-5","name":"first","date":"2017-01-05T19:46:59.933Z"},"recent":{"code":"NEW+2","name":"new","date":"2016-12-29T06:53:18.964Z"}}}
+
+$ witch-clock -j | python -m json.tool
+{
+    "lunar": {
+        "recent": {
+            "code": "NEW+2",
+            "date": "2016-12-29T06:53:18.964Z",
+            "name": "new"
+        },
+        "soonest": {
+            "code": "FIRST-5",
+            "date": "2017-01-05T19:46:59.933Z",
+            "name": "first"
+        }
+    },
+    "solar": {
+        "recent": {
+            "code": "WS+10",
+            "date": "2016-12-21T10:44:19.494Z",
+            "name": "winter solstice"
+        },
+        "soonest": {
+            "code": "VE-79",
+            "date": "2017-03-20T10:28:52.704Z",
+            "name": "vernal equinox"
+        }
+    }
+}
+
 ```
 
-Format is: `[soonest solar], [recent solar], [soonest lunar], [recent lunar]`.
+In the default and verbose results, the order of events goes: `[soonest solar], [recent solar], [soonest lunar], [recent lunar]`.
 
-The format with `-v` presents shortcodes, but also the datetime of the event in question.
+The `-v` option prints the shortcode, name, and date of each event. the `-j` option presents verbose info as JSON.
+
+Or, I mean, you could type `witch-clock -h` to see what's up.
 
 ## Usage, interpersonal
 
-witch-clock expresses dates like "VE-80" or "SS+12" but telling your friend "it's vee-ee eighty" can be hard to say and easily misheard. So I say it a lil differently:
+witch-clock expresses dates like "VE-80" or "SS+12" but telling your friend "it's vee-ee eighty" can be hard to say and easily misheard. So instead try:
 
 - VE: "vern", SS: "sum", AE: "auto", WS: "win"
 - "-": "sub", "+": "plus"
