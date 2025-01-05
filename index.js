ef12b2c7c8eed4479072df7a222f923b9addafb5
Diana Thayer <garbados@gmail.com>
2025-01-04 16:41:07 -0800

    refactor

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { solstice, moonphase } from "astronomia"
-import { meanSiderealYear } from "astronomia/base"
-import { JDEToDate, Calendar } from "astronomia/julian"
-import { Sunrise } from "astronomia/sunrise"
+import { solstice, moonphase } from 'astronomia'
+import { meanSiderealYear } from 'astronomia/base'
+import { JDEToDate, Calendar } from 'astronomia/julian'
+import { Sunrise } from 'astronomia/sunrise'
 
 const HOUR_IN_MS = 60 * 60 * 1000
 const DAY_IN_MS = 24 * HOUR_IN_MS
+const MONTH_IN_MS = moonphase.meanLunarMonth * DAY_IN_MS
 const YEAR_IN_MS = meanSiderealYear * DAY_IN_MS
 
 function dateToYearFraction (date) {
@@ -22,14 +23,18 @@ function wrapAstro (func) {
   }
 }
 
-export const seasons = {
+export const SEASONS = ['spring', 'summer', 'autumn', 'winter']
+
+const seasons = {
   spring: wrapAstro(solstice.march),
   summer: wrapAstro(solstice.june),
   autumn: wrapAstro(solstice.september),
   winter: wrapAstro(solstice.december)
 }
 
-export const phases = {
+export const PHASES = ['new', 'waxing', 'full', 'waning']
+
+const phases = {
   new: wrapAstro(moonphase.new),
   waxing: wrapAstro(moonphase.first),
   full: wrapAstro(moonphase.full),
@@ -38,87 +43,105 @@ export const phases = {
 
 function getPhaseInfo (date) {
   return Object.entries(phases).map(([phase, func]) => {
-    return [phase, func(date)]
+    return [phase, func(new Date(date.toLocaleDateString()))]
   })
 }
 
 function getSeasonInfo (date) {
   return Object.entries(seasons).map(([season, func]) => {
-    return [season, func(date)]
+    const yearStart = new Date(date.toLocaleDateString())
+    yearStart.setFullYear(date.getFullYear())
+    yearStart.setMonth(0)
+    yearStart.setDate(1 - moonphase.meanLunarMonth)
+    return [season, func(yearStart)]
   })
 }
 
-const months = [
+export const MONTHS = [
   'Jester',
   'Wizard',
   'Diviner',
   'Monarch',
-  'Despot',
+  'Steward',
   'Hierophant',
   'Lover',
-  'Chariot',
-  'Justiciar',
+  'Ranger',
+  'Warrior',
   'Hermit',
   'Trader',
   'Nomad',
-  'Hanged'
+  'Corpse'
 ]
 
-export function witchify (date, lat, long) {
-  if (date === undefined) date = new Date();
+export function getDateInfo (date, lat, long) {
+  if (date === undefined) date = new Date()
   long = -1 * long // astronomia counts westward, which is kinda backwards i guess
   const witchy = {}
   // season info
   const priorYear = new Date(date.getTime() - YEAR_IN_MS)
-  const seasonDateInfo = getSeasonInfo(date).concat(getSeasonInfo(priorYear))
-    .map(([season, seasonDate]) => { return [season, seasonDate, date - seasonDate]})
-  const [currentSeason, currentSeasonDate] = seasonDateInfo
+  const nextYear = new Date(date.getTime() + YEAR_IN_MS)
+  const seasonDateInfo = getSeasonInfo(date)
+    .concat(getSeasonInfo(priorYear))
+    .concat(getSeasonInfo(nextYear))
+    .map(([season, seasonDate]) => [season, seasonDate, date - seasonDate])
+  const currentSeasonInfo = seasonDateInfo
     .filter(([_season, _date, delta]) => { return delta > 0 })
-    .toSorted((a, b) => { return a[2] - b[2] })[0].slice(0, 2)
-  const [upcomingSeason, upcomingSeasonDate] = seasonDateInfo
+    .toSorted((a, b) => a[2] - b[2])[0].slice(0, 2)
+  const upcomingSeasonInfo = seasonDateInfo
     .filter(([_season, _date, delta]) => { return delta <= 0 })
-    .toSorted((a, b) => { return b[2] - a[2] })[0].slice(0, 2)
+    .toSorted((a, b) => b[2] - a[2])[0].slice(0, 2)
+  const currentSeasonDate = Math.ceil((date - currentSeasonInfo[1]) / DAY_IN_MS)
+  const daysLeftInSeason = Math.floor((upcomingSeasonInfo[1] - date) / DAY_IN_MS)
   witchy.season = {
-    current: [currentSeason, currentSeasonDate],
-    upcoming: [upcomingSeason, upcomingSeasonDate]
+    current: currentSeasonInfo,
+    upcoming: upcomingSeasonInfo,
+    date: currentSeasonDate,
+    rem: daysLeftInSeason
   }
   // phase info
-  const priorLunarMonth = new Date(date.getTime() - (moonphase.meanLunarMonth * DAY_IN_MS))
-  const phaseDateInfo = getPhaseInfo(date).concat(getPhaseInfo(priorLunarMonth))
-    .map(([phase, phaseDate]) => { return [phase, phaseDate, date - phaseDate]})
-  const [currentPhase, currentPhaseDate] = phaseDateInfo
-    .filter(([_phase, _date, delta]) => { return delta > 0 })
-    .toSorted((a, b) => a[2] - b[2])[0].slice(0, 2)
-  const [upcomingPhase, upcomingPhaseDate] = phaseDateInfo
-    .filter(([_phase, _date, delta]) => { return delta <= 0 })
-    .toSorted((a, b) => b[2] - a[2])[0].slice(0, 2)
-  witchy.phase = {
-    current: [currentPhase, currentPhaseDate],
-    upcoming: [upcomingPhase, upcomingPhaseDate]
-  }
-  // month info
-  const nextLunarMonth = new Date(date.getTime() + (moonphase.meanLunarMonth * DAY_IN_MS))
-  const firstMonthStart = phases.new(new Date(date.getFullYear(), 0, 1))
-  const monthsSinceStart = Math.floor((date - firstMonthStart) / (moonphase.meanLunarMonth * DAY_IN_MS))
-  const [monthStart, monthEnd] = getPhaseInfo(date)
+  const priorLunarMonth = new Date(date.getTime() - MONTH_IN_MS)
+  const nextLunarMonth = new Date(date.getTime() + MONTH_IN_MS)
+  const phaseDateInfo = getPhaseInfo(date)
     .concat(getPhaseInfo(priorLunarMonth))
     .concat(getPhaseInfo(nextLunarMonth))
-    .map(([phase, phaseDate]) => { return [phase, phaseDate, date - phaseDate]})
-    .filter(([phase, ..._rest]) => phase === 'new')
-    .toSorted((a, b) => b[2] - a[2])
-    .map(([_phase, phaseDate, _delta]) => phaseDate)
-    .slice(1, 3)
-  const daysSinceMonthStart = Math.floor((date - monthStart) / DAY_IN_MS)
+    .map(([phase, phaseDate]) => { return [phase, phaseDate, date - phaseDate] })
+  const currentPhaseInfo = phaseDateInfo
+    .filter(([_phase, _date, delta]) => { return delta > 0 })
+    .toSorted((a, b) => a[2] - b[2])[0].slice(0, 2)
+  const upcomingPhaseInfo = phaseDateInfo
+    .filter(([_phase, _date, delta]) => { return delta <= 0 })
+    .toSorted((a, b) => b[2] - a[2])[0].slice(0, 2)
+  const currentPhaseDate = Math.ceil((date - currentPhaseInfo[1]) / DAY_IN_MS)
+  const daysLeftInPhase = Math.floor((upcomingPhaseInfo[1] - date) / DAY_IN_MS)
+  witchy.phase = {
+    current: currentPhaseInfo,
+    upcoming: upcomingPhaseInfo,
+    date: currentPhaseDate,
+    rem: daysLeftInPhase
+  }
+  // month info
+  const firstMonthStart = phases.new(new Date(date.getFullYear(), 0, 1))
+  const monthsSinceStart = Math.floor((date - firstMonthStart) / MONTH_IN_MS)
+  const monthStart = new Date(firstMonthStart.getTime() + (MONTH_IN_MS * monthsSinceStart))
+  const monthEnd = new Date(monthStart.getTime() + MONTH_IN_MS)
+  const daysSinceMonthStart = Math.ceil((date - monthStart) / DAY_IN_MS)
+  const daysLeftInMonth = Math.floor((monthEnd - date) / DAY_IN_MS)
+  let nextMonth
+  if ((monthsSinceStart === 11) && witchy.season.current[0] === 'winter') {
+    nextMonth = MONTHS[0]
+  } else {
+    nextMonth = MONTHS[monthsSinceStart + 1] || MONTHS[0]
+  }
   witchy.month = {
-    name: months[monthsSinceStart],
-    next: months[monthsSinceStart + 1],
+    name: MONTHS[monthsSinceStart],
+    next: nextMonth,
     start: monthStart,
     end: monthEnd,
-    date: daysSinceMonthStart
+    date: daysSinceMonthStart,
+    rem: daysLeftInMonth
   }
   // day info
-  const yesterday = new Date(date)
-  yesterday.setDate(date.getDate() - 1)
+  const yesterday = new Date(date.getTime() - (DAY_IN_MS / 2))
   const sunrise = new Sunrise(new Calendar(yesterday), lat, long)
   const tomorrow = new Date(date)
   tomorrow.setDate(date.getDate() + 1)
@@ -128,20 +151,7 @@ export function witchify (date, lat, long) {
     set: sunrise.set().toDate(),
     next: nextSunrise.rise().toDate()
   }
-  return witchy
-}
-
-function capitalize (s) {
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
-
-export function dateText (date, lat, long) {
-  const witchy = witchify(date, lat, long)
-  const currentPhaseDate = Math.floor((date - witchy.phase.current[1]) / DAY_IN_MS)
-  const daysLeftInPhase = Math.floor((witchy.phase.upcoming[1] - date) / DAY_IN_MS)
-  const currentSeasonDate = Math.floor((date - witchy.season.current[1]) / DAY_IN_MS)
-  const daysLeftInSeason = Math.floor((witchy.season.upcoming[1] - date) / DAY_IN_MS)
-  const daysLeftInMonth = Math.floor((witchy.month.end - date) / DAY_IN_MS)
+  // time info
   const dayHourLength = (witchy.day.set - witchy.day.rise) / 10 // ten sunlight hours
   const nightHourLength = (witchy.day.next - witchy.day.set) / 10 // ten nighttime hours
   let hour, minute, second
@@ -160,13 +170,28 @@ export function dateText (date, lat, long) {
     minute = rawMinutes.slice(0, 2)
     second = rawMinutes.slice(2, 4)
   }
-  const txt = ""
-    + `It is day ${currentSeasonDate} of ${capitalize(witchy.season.current[0])}, ${daysLeftInSeason} til ${capitalize(witchy.season.upcoming[0])}.`
-    + " "
-    + `It is day ${currentPhaseDate} of the ${capitalize(witchy.phase.current[0])} Moon, ${daysLeftInPhase} til ${capitalize(witchy.phase.upcoming[0])}.`
-    + " "
-    + `It is day ${witchy.month.date} of the ${witchy.month.name}'s Moon; ${daysLeftInMonth} til the ${capitalize(witchy.month.next)}'s.`
-    + " "
-    + `The current time is ${hour}:${minute}:${second}, or ${date.toLocaleTimeString()}.`
+  witchy.time = {
+    hour,
+    minute: parseInt(minute, 10),
+    second: parseInt(second, 10),
+    str: `${hour}:${minute}:${second}`
+  }
+  return witchy
+}
+
+function capitalize (s) {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+export function getDateText (date, lat, long) {
+  const witchy = getDateInfo(date, lat, long)
+  const txt = '' +
+    `It is day ${witchy.season.date} of ${capitalize(witchy.season.current[0])}, ${witchy.season.rem} til ${capitalize(witchy.season.upcoming[0])}.` +
+    ' ' +
+    `It is day ${witchy.phase.date} of the ${capitalize(witchy.phase.current[0])} Moon, ${witchy.phase.rem} til ${capitalize(witchy.phase.upcoming[0])}.` +
+    ' ' +
+    `It is day ${witchy.month.date} of the ${witchy.month.name}'s Moon, ${witchy.month.rem} til the ${capitalize(witchy.month.next)}'s.` +
+    ' ' +
+    `The current time is ${witchy.time.str}, or ${date.toLocaleTimeString()}.`
   return txt
 }
---
+++ test.js
@@ -0,0 +1,44 @@
+/* global describe, it */
+import * as assert from 'assert/strict'
+import { getDateInfo, SEASONS, PHASES, MONTHS } from './index.js'
+import * as fc from 'fast-check'
+
+function isDateValid (d) {
+  return !isNaN(d.getTime())
+}
+
+describe('witch-clock', function () {
+  it('should handle arbitrary dates, latitudes, and longitudes', function () {
+    const baseOptions = { noNaN: true, noDefaultInfinity: true, }
+    fc.assert(
+      fc.property(
+        fc.date({ noInvalidDate: true, min: new Date(1000, 0, 1), max: new Date(3000, 0, 1) }),
+        fc.float({ min: Math.fround(-89.6), max: Math.fround(89.6), ...baseOptions }),
+        fc.float({ min: -180, max: 180, ...baseOptions }),
+        (date, lat, long) => {
+          const witchy = getDateInfo(date, lat, long)
+          // season tests
+          const [currentSeason, currentSeasonDate] = witchy.season.current
+          assert.ok(SEASONS.includes(currentSeason), `Unknown season: ${currentSeason}`)
+          assert.ok(isDateValid(currentSeasonDate))
+          const [upcomingSeason, upcomingSeasonDate] = witchy.season.upcoming
+          assert.ok(SEASONS.includes(upcomingSeason), `Unknown season: ${upcomingSeason}`)
+          assert.ok(isDateValid(upcomingSeasonDate))
+          assert.ok(witchy.season.date > 0)
+          assert.ok(witchy.season.rem >= 0)
+          // phase tests
+          const [currentPhase, currentPhaseDate] = witchy.phase.current
+          assert.ok(PHASES.includes(currentPhase), `Unknown season: ${currentPhase}`)
+          assert.ok(isDateValid(currentPhaseDate))
+          const [upcomingPhase, upcomingPhaseDate] = witchy.phase.upcoming
+          assert.ok(PHASES.includes(upcomingPhase), `Unknown season: ${upcomingPhase}`)
+          assert.ok(isDateValid(upcomingPhaseDate))
+          assert.ok(witchy.phase.date > 0)
+          assert.ok(witchy.phase.rem >= 0)
+          // TODO month checks
+          // TODO time checks
+        }
+      )
+    )
+  })
+})
